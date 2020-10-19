a0258321b8b1a3c51b316c38cda79140e0a2854a
DarioAlejandroS <73087251+DarioAlejandroS@users.noreply.github.com>
2020-10-19 01:47:07 -0300

    Add files via upload

---
+++ Python/queries.py
@@ -0,0 +1,33 @@
+import pandas
+import psycopg2
+
+conn = psycopg2.connect(
+    host="172.18.0.2",
+    database="db",
+    user="root",
+    password="pos")
+    
+cur = conn.cursor()
+    
+print('PostgreSQL database version:')
+cur.execute('SELECT version()')
+
+# display the PostgreSQL database server version
+db_version = cur.fetchone()
+print(db_version)
+
+cur = conn.cursor()
+cur.execute("SELECT koi_disposition, koi_score, koi_depth, koi_prad, koi_steff FROM exo")
+rows = cur.fetchall()
+
+for row in rows:
+    print("ADMISSION =", row[0])
+    print("NAME =", row[1])
+    print("AGE =", row[2])
+    print("COURSE =", row[3])
+    print("DEPARTMENT =", row[4], "\n")
+
+print("Operation done successfully")
+
+# close the communication with the PostgreSQL
+cur.close()
