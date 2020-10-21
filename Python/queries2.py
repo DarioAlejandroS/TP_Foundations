9a2abb749392ddfef251c2dfa24dffcf50f37b60
DarioAlejandroS <73087251+DarioAlejandroS@users.noreply.github.com>
2020-10-20 22:00:29 -0300

    Update queries2.py

--- Python/queries2.py
+++ Python/queries2.py
@@ -34,3 +34,10 @@ argentinosfinalizado=argentinos.groupby(by=["status"]).count()
 
 plt.bar()
 plt.show()
+
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+langs = ['C', 'C++', 'Java', 'Python', 'PHP']
+students = [23,17,35,29,12]
+ax.bar(langs,students)
+plt.show()
