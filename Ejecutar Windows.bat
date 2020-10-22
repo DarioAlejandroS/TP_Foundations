cabc887980ec91d21b133ad66b2ab87724f408e2
DarioAlejandroS <73087251+DarioAlejandroS@users.noreply.github.com>
2020-10-22 20:33:33 -0300

    6.1

--- Ejecutar Windows.bat
+++ Ejecutar Windows.bat
@@ -1,10 +1,11 @@
-set volumen=C:/Users/Dario/Desktop/Vol
+:: Cambiar la variable vol1 por una ruta local donde se alojaran los archivos
+set vol1=C:/Users/Dario/Desktop/Vol
 cd PostgreSQL
 docker build -t posi .
 cd ..
 cd Python
 docker build -t pythoni .
 cd ..
-docker volume create --driver local --opt type=nfs --opt device=:%volumen% myvolume
+docker volume create --driver local --opt type=none --opt device=%vol1% --opt o=bind myvolume
 docker-compose up -d
 start http://localhost:5000
