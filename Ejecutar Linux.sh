9ef8185adaabaf7906b53da37b93dd816d4efe90
DarioAlejandroS <dario.sella@gmail.com>
2020-10-23 10:55:56 -0300

    Agregado de "Ejecutar Linux.sh"

---
+++ Ejecutar Linux.sh
@@ -0,0 +1,10 @@
+vol1=/home/dario/Desktop/Docker
+cd PostgreSQL
+docker build -t posi .
+cd ..
+cd Python
+docker build -t pythoni .
+cd ..
+docker volume create --driver local --opt type=none --opt device=$vol1 --opt o=bind myvolume
+docker-compose up -d
+curl http://localhost:5000
